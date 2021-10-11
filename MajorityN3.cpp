d72f6d51871197fff8dde6ae1e806c7a2162beeb
Biswa5812 <44234453+Biswa5812@users.noreply.github.com>
2021-10-11 09:27:37 +0530

    Add files via upload

---
+++ MajorityN3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        v.push_back(a);
+    }
+    
+}
